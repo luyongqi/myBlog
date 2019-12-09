3140340714d961025a7aa0caee0b8e2264e2fd41
luyongqi <2357356553@qq.com>
2019-12-09 22:16:39 +0800

    修改首页

--- my-client/src/views/layout/Layout.vue
+++ my-client/src/views/layout/Layout.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="app-wrapper" :class="classObj">
     <Header></Header>
-    <div class="main-container">
+    <section class="main-container">
       <app-main></app-main>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain } from './components'
+import { AppMain } from './components'
 // import ResizeMixin from './mixin/ResizeHandler'
 import Header from './components/Header'
-import {getMenuList,getMenuListAll} from '@/api/menu';
 
 export default {
   name: 'layout',
   components: {
     Header,   //头部导航
-    Navbar, //二级navbar 放页面当前位置的
-    Sidebar, //左侧导航
     AppMain, //内容组件
   },
   // mixins: [ResizeMixin],
@@ -26,32 +23,17 @@ export default {
    
   },
   created(){
-    //这里可以请求菜单,然后存入state 避免重复请求 后面权限需要设置菜单的显示与隐藏
-    getMenuList().then(res=>{
-
-      console.log(res,'这里是菜单(权限)的获取')
-    })
-
-    getMenuListAll().then(res=>{
-
-      console.log(res,'这里是菜单(所有)的获取')
-
-    })
-    
+  
   }
 }
-
-
 
 
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  @import "src/styles/mixin.scss";
   .app-wrapper {
-    @include clearfix;
-    position: relative;
-    height: 100%;
-    width: 100%;
+    max-width: 1200px;
+    margin:auto;
+    margin-top: 60px;
   }
 </style>
