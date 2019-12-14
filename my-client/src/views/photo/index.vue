9734ee8f0cf7a43df90fd50eace45dd11678f3f6
luyongqi <2357356553@qq.com>
2019-12-14 17:58:32 +0800

    相册

--- my-client/src/views/photo/index.vue
+++ my-client/src/views/photo/index.vue
@@ -45,10 +45,6 @@
   </div>
 </template>
 
-
-<!-- 页面滚动显示动画效果，IE10 以下无效 -->
-
-
 <script>
 import scrollReveal from 'scrollreveal';
 
@@ -61,26 +57,27 @@ export default {
   },
  
   mounted() {
+  
     window.addEventListener('scroll', ()=>{
-    
+
       this.scrollReveal.reveal('.reveal-top', {
-      // 动画的时长
-      duration: 2000,
-      // 延迟时间
-      delay: 500,
-      // 动画开始的位置，'bottom', 'left', 'top', 'right'
-      origin: 'top',
-      // 回滚的时候是否再次触发动画
-      reset: true,
-      // 在移动端是否使用动画
-      mobile: true,
-      // 滚动的距离，单位可以用%，rem等
-      distance: '200px',
-      // 其他可用的动画效果
-      opacity: 0.001,
-      easing: 'linear',
-      scale: 0.9,
-    });
+        // 动画的时长
+        duration: 2000,
+        // 延迟时间
+        delay: 500,
+        // 动画开始的位置，'bottom', 'left', 'top', 'right'
+        origin: 'top',
+        // 回滚的时候是否再次触发动画
+        reset: true,
+        // 在移动端是否使用动画
+        mobile: true,
+        // 滚动的距离，单位可以用%，rem等
+        distance: '200px',
+        // 其他可用的动画效果
+        opacity: 0.001,
+        easing: 'linear',
+        scale: 0.9,
+      });
   
     })
     
