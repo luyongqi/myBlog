a1986f5f6229e24149eedab6fe78f88a03f0e942
luyongqi <2357356553@qq.com>
2020-05-29 11:17:17 +0800

    egg中台 静态访问文件设置

--- myService/config/config.default.js
+++ myService/config/config.default.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 卢勇其
  * @Date: 2020-05-22 19:45:39
- * @LastEditors: 卢勇其
- * @LastEditTime: 2020-05-24 13:59:40
+ * @LastEditors: your name
+ * @LastEditTime: 2020-05-28 14:56:02
  * @Description: file content
  */ 
 /* eslint valid-jsdoc: "off" */
@@ -12,11 +12,15 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+
+
 module.exports = appInfo => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
+  const path = require('path')
+  
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
@@ -25,6 +29,16 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.static = {
+    // 静态化访问前缀,如：`http://127.0.0.1:7001/static/images/logo.png`
+    prefix: '/static', 
+    dir: path.join(appInfo.baseDir, 'app/public'), // `String` or `Array:[dir1, dir2, ...]` 静态化目录,可以设置多个静态化目录
+    dynamic: true, // 如果当前访问的静态资源没有缓存，则缓存静态文件，和`preload`配合使用；
+    preload: false,
+    maxAge: 31536000, // in prod env, 0 in other envs
+    buffer: true, // in prod env, false in other envs
+  };
+  
   config.mongoose = {
     url: 'mongodb://127.0.0.1/blogDB',
     options: {},
