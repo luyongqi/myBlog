a42437ebfa6f86fe3cf8b906f93e4a347a74c157
luyongqi <2357356553@qq.com>
2020-06-18 23:29:33 +0800

    notfound

--- my-admin/src/Pages/notFound/index.js
+++ my-admin/src/Pages/notFound/index.js
@@ -1,12 +1,12 @@
 /*
  * @Author: 卢勇其
  * @Date: 2020-06-11 19:26:22
- * @LastEditors: your name
- * @LastEditTime: 2020-06-13 10:43:35
+ * @LastEditors: 卢勇其
+ * @LastEditTime: 2020-06-18 23:29:02
  * @Description: 工作台
  */ 
 
-import React, { useState } from './node_modules/react';
+import React, { useState } from 'react';
 
 function NotFound(){
    const [data,setData] = useState('')
